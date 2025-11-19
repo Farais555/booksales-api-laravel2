78933b94cd7a06905323d6b9538153c1c56800a7
Farais555 <romimfarais354@gmail.com>
2025-11-19 23:47:39 +0700

    nambahin jawa web token dan role, login logout

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.admin',
+            'password' => bcrypt('admin123'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Customer',
+            'email' => 'customer@custom.mer',
+            'password' => bcrypt('customer123'),
+            'role' => 'customer'
+        ]);
+    }
+}

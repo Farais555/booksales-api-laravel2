beeea953a5f1ce9355f8494978880f5cb1bfad9b
Farais555 <romimfarais354@gmail.com>
2025-11-19 20:43:59 +0700

    seting route author dan genre

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
+
+    // tampilkan semua data
     public function index() {
         $authors = Author::all();
         
@@ -25,7 +28,9 @@ class AuthorController extends Controller
         ], 200);
     }
 
-        public function store(Request $request) {
+
+    // upload data
+    public function store(Request $request) {
         // 1. validator
         $validator = Validator::make($request->all(), [
             'name' => 'required|string',
@@ -58,5 +63,97 @@ class AuthorController extends Controller
             'message' => 'Resource added successfuly!',
             'data' => $author
         ], 201);
+    }
+
+    // cari salah satu data
+    public function show(string $id) {
+        $author = Author::find($id);
+
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => "Resource not found"
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail resource',
+            'data' => $author
+        ], 200);
+    }
+
+    // update data
+    public function update(string $id, Request $request) {
+        // 1. mencari data
+        $author = Author::find($id);
+
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => "Resource not found"
+            ], 404);
+        }        
+
+        // 2. validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'photo' => 'nullable|image|mimes:jpeg,jpg,png|max:4096',
+            'bio' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+
+        // 3. siapkan data yang ingin diupdate
+        $data = [
+            'name' => $request->name,
+            'bio' => $request->bio
+        ];
+
+        // 4. handle image (upload & delete image)
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+            $image->store('authors', 'public');
+
+            if ($author->photo) {
+                Storage::disk('public')->delete('authors/' . $author->photo);
+            }
+
+            $data['photo'] = $image->hashName();
+        }
+
+        // 5. updata data baru ke database
+        $author->update($data);
+
+        //  response saat data berhasil dirubah
+        return response()->json([
+            'success' => true,
+            'message' => 'Resource updated successfuly!',
+            'data' => $author
+        ], 200);
+    }
+
+    // delete data
+    public function destroy(string $id) {
+        $author = Author::find($id);
+
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => "Resource not found"
+            ], 404);
+        }
+
+        if ($author->photo) {
+            // delete dari storage
+            Storage::disk('public')->delete('authors/' . $author->photo);
+        }
+
+        $author->delete();
     }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
+    // tampilkan semua data
     public function index() {
         $genres = Genre::all();
 
@@ -25,7 +26,8 @@ class GenreController extends Controller
         ], 200);
     }
 
-        public function store(Request $request) {
+    // upload data
+    public function store(Request $request) {
         // 1. validator
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:100',
@@ -57,4 +59,96 @@ class GenreController extends Controller
             'data' => $genre
         ], 201);
     }
+
+    // cara salah satu data
+    public function show(string $id) {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => "Resource not found"
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail resource',
+            'data' => $genre
+        ], 200);
+    }
+
+    // update data
+    public function update(string $id, Request $request) {
+        // 1. mencari data
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => "Resource not found"
+            ], 404);
+        }        
+
+        // 2. validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+
+        // 3. siapkan data yang ingin diupdate
+        $data = [
+            'name' => $request->name,
+            'description' => $request->description
+        ];
+
+        // // 4. handle image (upload & delete image)
+        // if ($request->hasFile('photo')) {
+        //     $image = $request->file('photo');
+        //     $image->store('authors', 'public');
+
+        //     if ($author->photo) {
+        //         Storage::disk('public')->delete('authors/' . $author->photo);
+        //     }
+
+        //     $data['photo'] = $image->hashName();
+        // }
+
+        // 5. updata data baru ke database
+        $genre->update($data);
+
+        //  response saat data berhasil dirubah
+        return response()->json([
+            'success' => true,
+            'message' => 'Resource updated successfuly!',
+            'data' => $genre
+        ], 200);
+    }
+
+    // delete data
+    public function destroy(string $id) {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => "Resource not found"
+            ], 404);
+        }
+
+        // if ($author->photo) {
+        //     // delete dari storage
+        //     Storage::disk('public')->delete('authors/' . $author->photo);
+        // }
+
+        $genre->delete();
+    }
 }
+
